c462011b9a5f01fa0697ac239e027e00adef67aa
Pater999 <39135429+Pater999@users.noreply.github.com>
2020-11-05 19:41:05 +0100

    localhost CORS bypass added

--- backend/app.ts
+++ backend/app.ts
@@ -7,7 +7,15 @@ import { admin } from './admin';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
+// CORS BYPASS
+app.use((req: any, res: express.Response, next: express.NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
 
 /**
  * Middleware to manage authentication

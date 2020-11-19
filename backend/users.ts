65df315e2254716e974bdecc95ba0ea89cf41fcc
Pater999 <39135429+Pater999@users.noreply.github.com>
2020-11-19 15:42:51 +0100

    Rimossa api login user inutile

--- backend/users.ts
+++ backend/users.ts
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import { RegisterDto } from './models/DTO/RegisterDTO';
 import Users from './Database/schemas/users';
-import { FakeDatabase } from './Database/fakeDatabase';
-import { validateTokenAdmin } from './Utilities/authentication';
 import { connUri, dbOptions, stage } from './Database/databaseService';
 
 
@@ -35,35 +33,5 @@ router.post('', async (req: express.Request, res: express.Response) => {
     }
   }
 });
-
-//#region LoginUser
-router.post('/login', (req: any, res: express.Response) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (!username) {
-    res.status(400).json({ error: 'Campo Utente vuoto o null' });
-    return;
-  }
-
-  if (!password) {
-    res.status(400).json({ error: 'Campo Password vuoto o null' });
-    return;
-  }
-
-  if (username == FakeDatabase.User.username && password == FakeDatabase.User.password) {
-    const token = Buffer.from(FakeDatabase.User.username + Date.now().toString()).toString('base64');
-    res.status(200).json({ token, username });
-    return;
-  } else {
-    res.status(401).json({ error: 'Username o Password non corretti!' });
-  }
-});
-
-router.get('/users', validateTokenAdmin, (req: any, res: express.Response) => {
-  res.status(200).json(FakeDatabase.User);
-});
-
-//#endregion
 
 export { router as users };

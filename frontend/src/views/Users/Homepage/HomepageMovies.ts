c02afdd912dd00a7ad57312fd006c75d0a18da25
Francesco Previdi <francesco.previdi@gmail.com>
2020-11-20 20:46:33 +0100

    Storia #9 Styling fixed

--- frontend/src/views/Users/Homepage/HomepageMovies.ts
+++ frontend/src/views/Users/Homepage/HomepageMovies.ts
@@ -1,16 +1,17 @@
 import axiosInstance from "@/axios-instance";
-import MovieDTO from '@/Models/MovieDTO';
-import Header from '@/components/Header/Header.vue';
+import MovieDTO from "@/Models/MovieDTO";
+import Header from "@/components/Header/Header.vue";
 
 import { Component, Vue } from "vue-property-decorator";
 
+
 @Component({ components: { Header } })
 export default class HomepageMovies extends Vue {
+  
   isLoading = false;
   movies: MovieDTO[] = [];
-
-  dateTimeSelected = new Date(Date.now())
-
+  dateTimeSelected = new Date(Date.now());
+ 
   async mounted() {
     await this.getMovies();
   }
@@ -18,24 +19,26 @@ export default class HomepageMovies extends Vue {
   async getMovies() {
     this.isLoading = true;
     try {
-        const response = await axiosInstance.get(`/movies?date=${this.dateTimeSelected.toUTCString()}`);
-        this.movies = (response.data as MovieDTO[]);
-    }
-    catch (error) {
+      const response = await axiosInstance.get(
+        `/movies?date=${this.dateTimeSelected.toUTCString()}`
+      );
+      this.movies = response.data as MovieDTO[];
+    } catch (error) {
       if (error.response && error.response.data && error.response.data.error)
         this.$message.error(error.response.data.error);
-    }
-    finally {
+    } finally {
       this.isLoading = false;
     }
   }
 
-  async dateChanged()
-  {
-    if (this.dateTimeSelected)
-    {
+  async dateChanged() {
+    if (this.dateTimeSelected) {
       this.movies = [];
-      await this.getMovies();  
+      await this.getMovies();
     }
   }
+
 }
+
+
+
--- frontend/src/views/Users/Homepage/HomepageMovies.vue
+++ frontend/src/views/Users/Homepage/HomepageMovies.vue
@@ -1,98 +1,47 @@
 <template>
   <CWrapper>
     <Header />
-    <div>
+    <div v-loading="isLoading" class="container mt-3">
+      <el-date-picker
+        class="mt-4"
+        lang="it"
+        v-model="dateTimeSelected"
+        type="date"
+        @change="dateChanged()"
+      ></el-date-picker>
 
-      <div v-loading="isLoading">
-        <el-row>
-          <div
-            style="
-              float: left;
-              margin-left: 1%;
-              margin-bottom: 10px;
-              margin-top: 30px;
-            "
-          >
-          <h1>Film in programma</h1>
+      <div v-for="movie in movies" :key="movie.Id">
+        <div class="card">
+          <div class="row align-items-center">
+            <div class="col-md-3">
+              <img :src="movie.imageUrl" class="img-fluid" width="75%" />
+            </div>
+            <div class="col-md-8">
+              <h2 class="card-title mt-2">
+                {{ movie.title }}
+                <hr />
+              </h2>
+              <p>{{ movie.desc }}</p>
+            </div>
+
+            <div class="col">
+              <el-button
+                class="orario"
+                circle
+                v-for="plan in movie.plans"
+                :key="`${plan.Date}-${plan.room._id}`"
+              >
+                {{ new Date(plan.date).getHours() }}:{{ new Date(plan.date).getMinutes() }} <br />
+                {{ plan.room.name }}
+              </el-button>
+              <el-button class="prenota" type="primary">PRENOTA ORA</el-button>
+            </div>
           </div>
-          <div
-            style="
-              float: right;
-              margin-right: 1%;
-              margin-bottom: 10px;
-              margin-top: 10px;
-            "
-          >
-            <el-date-picker
-              class="mt-4"
-              lang="it"
-              v-model="dateTimeSelected" 
-              type="date"
-              @change="dateChanged()"
-            ></el-date-picker>
-          </div>
-        </el-row>
-        <div v-for="movie in movies" :key="movie._id">
-              <el-card style="width: 98%">
-                <template>
-                  <div style="width: 100%" class="content">
-                    <img
-                      class="mr-3 roundedFilm"
-                      :src="movie.imageUrl"
-                      width="12%"
-                    />
-                    <el-col :span="0.5" style="margin-left: 50px"> </el-col>
-                    <el-col class="testo">
-                      <el-row
-                        ><span class="testo" style="font-size:18.0pt">{{
-                          movie.title
-                        }}</span></el-row
-                      >
-                      <div class="line"></div>
-                      <el-row>
-                        <span class="testo" style="font-size:12.0pt">{{
-                          movie.desc
-                        }}</span>
-                      </el-row>
-                    </el-col>
-
-                    <el-col>
-                      <el-row>
-                        <el-button
-                          class="orario"
-                          circle
-                          v-for="plan in movie.plans"
-                          :key="`${plan.date}-${plan.room._id}`"
-                        >
-                          {{ new Date(plan.date).getHours() }}:{{
-                            new Date(plan.date).getMinutes()
-                          }}<div>{{ plan.room.name }}</div></el-button
-                        >
-                      </el-row>
-                      <el-row>
-                        <el-button
-                          style="float: right; margin-top: 120px"
-                          type="warning"
-                          >PRENOTA ORA</el-button
-                        >
-
-                        
-                        
-                      </el-row>
-                    </el-col>
-                  </div>
-                </template>
-              </el-card>
         </div>
       </div>
     </div>
   </CWrapper>
 </template>
 
-
-
 <script lang="ts" src="./HomepageMovies.ts" />
 <style lang="scss" src="./HomepageMovies.scss" />
-
-
-

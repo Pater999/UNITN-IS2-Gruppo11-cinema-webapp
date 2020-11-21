a9cdb9d81efd149be729493b51dde82fac7f8eee
Pater999 <39135429+Pater999@users.noreply.github.com>
2020-11-21 16:08:36 +0100

    heroku crash fix

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
 import { app } from './backend/app';
 import { config } from './config';
-import logger from 'morgan';
 
 const environment = process.env.NODE_ENV!;
 const stage = config[environment];
-
-if (environment !== 'production') {
-  app.use(logger('dev'));
-}
 
 app.listen(`${stage.port}`, () => {
   console.log(`Server listening on port ${stage.port}`);

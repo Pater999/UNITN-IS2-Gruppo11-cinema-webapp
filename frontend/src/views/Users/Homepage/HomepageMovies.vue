b8bad19bac22498d915decb2f42e77656eb04315
Francesco Previdi <francesco.previdi@gmail.com>
2020-11-22 13:49:09 +0100

    Storia #9 responsive

--- frontend/src/views/Users/Homepage/HomepageMovies.vue
+++ frontend/src/views/Users/Homepage/HomepageMovies.vue
@@ -1,40 +1,50 @@
 <template>
   <CWrapper>
     <Header />
-    <div v-loading="isLoading" class="container mt-3">
-      <el-date-picker
-        class="mt-4"
-        lang="it"
-        v-model="dateTimeSelected"
-        type="date"
-        @change="dateChanged()"
-      ></el-date-picker>
+    <div v-loading="isLoading" class="container">
+      <div id="c" class=" d-flex flex-row-reverse align-items-start">
+        <el-date-picker
+          class="mt-4"
+          lang="it"
+          v-model="dateTimeSelected"
+          type="date"
+          @change="dateChanged()"
+        ></el-date-picker>
+      </div>
 
       <div v-for="movie in movies" :key="movie.Id">
-        <div class="card">
-          <div class="row align-items-center">
-            <div class="col-md-3">
-              <img :src="movie.imageUrl" class="img-fluid" width="75%" />
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-2 ">
+              <img  :src="movie.imageUrl" height="100%" class="card-img" />
             </div>
-            <div class="col-md-8">
-              <h2 class="card-title mt-2">
-                {{ movie.title }}
+            <div class="col-md-8 my-auto">
+              <div class="card-body">
+                <h2 class="card-title">{{ movie.title }}</h2>
                 <hr />
-              </h2>
-              <p>{{ movie.desc }}</p>
+                <p class="card-text text-justify">
+                  {{ movie.desc }}
+                </p>
+              </div>
             </div>
 
-            <div class="col">
-              <el-button
-                class="orario"
-                circle
-                v-for="plan in movie.plans"
-                :key="`${plan.Date}-${plan.room._id}`"
-              >
-                {{ new Date(plan.date).getHours() }}:{{ new Date(plan.date).getMinutes() }} <br />
-                {{ plan.room.name }}
-              </el-button>
-              <el-button class="prenota" type="primary">PRENOTA ORA</el-button>
+            <div class="col-md-2">
+              <div id="p" class=" h-75 d-flex flex-row-reverse align-items-start">
+                <el-button
+                  class="orario"
+                  circle
+                  v-for="plan in movie.plans"
+                  :key="`${plan.Date}-${plan.room._id}`"
+                >
+                  {{ new Date(plan.date).getHours() }}:{{ new Date(plan.date).getMinutes() }}
+                  <br />
+                  {{ plan.room.name }}
+                </el-button>
+              </div>
+
+              <div id="p" class="h-25 d-flex flex-row-reverse align-items-end">
+                <el-button type="primary">PRENOTA ORA</el-button>
+              </div>
             </div>
           </div>
         </div>

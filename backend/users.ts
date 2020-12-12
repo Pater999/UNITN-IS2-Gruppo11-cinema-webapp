65837868af50e37393433bf044a8a8c74b3d8e05
TommasoManzana <manzana.tommaso@gmail.com>
2020-12-12 16:06:47 +0100

    Storia #15 - Sviluppo API Lista prenotazioni utente (GET)

--- backend/users.ts
+++ backend/users.ts
@@ -1,10 +1,11 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-import { validateTokenAdmin } from './Utilities/authentication';
-import { RegisterDto } from './models/DTO/RegisterDTO';
-import Users from './Database/schemas/users';
-import { connUri, dbOptions, stage } from './Database/databaseService';
+import express from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { validateTokenAdmin, validateTokenUser } from "./Utilities/authentication";
+import { RegisterDto } from "./models/DTO/RegisterDTO";
+import Users from "./Database/schemas/users";
+import Movies from "./Database/schemas/movies";
+import { connUri, dbOptions, stage } from "./Database/databaseService";
 
 const router = express.Router();
 
@@ -12,7 +13,7 @@ router.get("", validateTokenAdmin, async (req: express.Request, res: express.Res
   let db = null;
   try {
     db = await mongoose.createConnection(connUri, dbOptions);
-    const UserMod = db.model('Users', Users);
+    const UserMod = db.model("Users", Users);
     const users = await UserMod.find().select("-__v -Password").exec();
     res.status(200).json(users);
   } catch (err) {
@@ -22,24 +23,24 @@ router.get("", validateTokenAdmin, async (req: express.Request, res: express.Res
   }
 });
 
-router.put('/:userId/change-role', validateTokenAdmin, async (req: express.Request, res: express.Response) => {
+router.put("/:userId/change-role", validateTokenAdmin, async (req: express.Request, res: express.Response) => {
   let id = req.params.userId;
-  if (!id) res.status(400).json({ error: 'Bad request' });
+  if (!id) res.status(400).json({ error: "Bad request" });
   else {
     let role = req.body.role;
 
-    if (!role || (role !== "admin" && role !== "User")) res.status(400).json({ error: 'Bad request' });
+    if (!role || (role !== "admin" && role !== "User")) res.status(400).json({ error: "Bad request" });
     else {
       let db = null;
       try {
         db = await mongoose.createConnection(connUri, dbOptions);
-        const UserMod = db.model('Users', Users);
-        const elem = (await UserMod.findByIdAndUpdate(id, { Role:role }, { new: true, useFindAndModify: false }).select("-__v -Password").exec()) as any;
+        const UserMod = db.model("Users", Users);
+        const elem = (await UserMod.findByIdAndUpdate(id, { Role: role }, { new: true, useFindAndModify: false }).select("-__v -Password").exec()) as any;
         if (!elem) throw new Error();
 
         res.status(200).json(elem);
       } catch (err) {
-        res.status(404).json({ error: 'User not found' });
+        res.status(404).json({ error: "User not found" });
       } finally {
         db && db.close();
       }
@@ -47,11 +48,11 @@ router.put('/:userId/change-role', validateTokenAdmin, async (req: express.Reque
   }
 });
 
-router.post('', async (req: express.Request, res: express.Response) => {
+router.post("", async (req: express.Request, res: express.Response) => {
   const { name, surname, username, email, password } = req.body;
 
   if (!name || !surname || !username || !email || !password) {
-    res.status(400).json({ error: 'Some Fields are null or empty!' });
+    res.status(400).json({ error: "Some Fields are null or empty!" });
   } else {
     try {
       const hashedPass = await bcrypt.hash(password, stage.saltingRounds);
@@ -59,17 +60,87 @@ router.post('', async (req: express.Request, res: express.Response) => {
       let db = null;
       try {
         db = await mongoose.createConnection(connUri, dbOptions);
-        const UserMod = db.model('Users', Users);
+        const UserMod = db.model("Users", Users);
         const { _id, Username, Role, Name, Surname, Email } = (await UserMod.create(elem)) as any;
         res.status(201).json({ id: _id, Username, Role, Name, Surname, Email });
       } catch (err) {
-        res.status(409).json({ error: 'Questo username è stato già utilizzato!' });
+        res.status(409).json({ error: "Questo username è stato già utilizzato!" });
       } finally {
         db && db.close();
       }
     } catch (error) {
-      res.status(500).json({ error: 'Internal server error.' });
+      res.status(500).json({ error: "Internal server error." });
     }
+  }
+});
+
+router.get("/:userId/reservations", validateTokenUser, async (req: express.Request, res: express.Response) => {
+  let userId = req.params.userId;
+  const tokenUsername = res.locals.username;
+
+  const query = [
+    { $unwind: "$plans" },
+    { $unwind: "$plans.reservations" },
+    {
+      $project: { _id: 0, reservationId: "$plans.reservations._id", date: "$plans.date", movieTitle: "$title", movieId: "$_id", userId: "$plans.reservations.userId", fareId: "$plans.reservations.fareId" },
+    },
+    {
+      $lookup: {
+        from: "fares",
+        localField: "fareId",
+        foreignField: "_id",
+        as: "fares",
+      },
+    },
+    {
+      $project: {
+        reservationId: 1,
+        date: 1,
+        movieTitle: 1,
+        movieId: 1,
+        fare: { $arrayElemAt: ["$fares", 0] },
+      },
+    },
+    {
+      $unset: ["fare.__v"],
+    },
+  ];
+
+  if (!userId) {
+    res.status(400).json({ error: "Bad request" });
+    return;
+  }
+
+  try {
+    query.unshift({ $match: { "plans.reservations.userId": new mongoose.Types.ObjectId(userId) } } as any);
+  } catch {
+    res.status(400).json({ error: "Bad request" });
+    return;
+  }
+
+  let db = null;
+  try {
+    db = await mongoose.createConnection(connUri, dbOptions);
+
+    const UserMod = db.model("Users", Users);
+    const user = await UserMod.findOne({ Username: tokenUsername });
+    if (!user) {
+      res.status(500).json({ error: "Internal server error." });
+      return;
+    } else if (user._id != userId) {
+      res.status(403).json({ error: "Non puoi visualizzare la lista biglietti di un altro utente" });
+      return;
+    }
+
+    const MovieMod = db.model("Movies", Movies);
+    let reservations = await MovieMod.aggregate(query);
+
+    res.status(200).json(reservations);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error." });
+  } finally {
+    db && db.close();
   }
 });
 

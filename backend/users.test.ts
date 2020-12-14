3fc65919e834f5b1e0b30674d0ce6c2bb014f416
Daniele <sopranidaniele23@gmail.com>
2020-12-14 17:41:01 +0100

    Storia #23 - #26 - Testing API User Info / Change Password

--- backend/users.test.ts
+++ backend/users.test.ts
@@ -78,3 +78,69 @@ describe('GET /api/v1/users/:userId/reservations', () => {
       });
   });
 });
+
+
+//Users Info
+describe("GET /api/v1/users/:userId/", () => {
+  let server: any;
+  let agent: request.SuperAgentTest;
+
+  beforeAll((done) => {
+    const port = process.env.PORT || 3000;
+
+    server = app.listen(port, () => {
+      agent = request.agent(server);
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  let userToken = createToken('PIPPO', 'user');
+  it("GET /api/v1/users/:userId should return 404 because the user does not exists", async (done) => {
+    request(server)
+      .get(`/api/v1/users/${mongoose.Types.ObjectId()}/`)
+      .set('Authorization', 'Bearer ' + userToken)
+      .send({ userId: mongoose.Types.ObjectId() })
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .end((err, res) => {
+        done();
+      });
+  });
+});
+
+//Users Change Password
+describe("PUT /api/v1/users/:userId/change-password", () => {
+  let server: any;
+  let agent: request.SuperAgentTest;
+
+  beforeAll((done) => {
+    const port = process.env.PORT || 3000;
+
+    server = app.listen(port, () => {
+      agent = request.agent(server);
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  let userToken = createToken('gigi', 'user');
+  it("PUT /api/v1/users/:userId/change-password should return 200 because the password was changed", async (done) => {
+    request(server)
+      .put(`/api/v1/users/${mongoose.Types.ObjectId()}/change-password`)
+      .set('Authorization', 'Bearer ' + userToken)
+      .send({ userId: mongoose.Types.ObjectId(), nuovaPassword: "1234" })
+      .expect('Content-Type', /json/)
+      .then((response) => 
+      {
+        expect(200);
+        done();
+      })
+  });
+});
